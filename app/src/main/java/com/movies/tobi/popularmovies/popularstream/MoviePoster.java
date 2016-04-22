b7fde68c69ec87c352ceb3628e3cee73611d866d
Tobias Heine <tobias@novoda.com>
2016-04-22 18:30:14 +0200

    Add MoviePoster class as data representation for the UI

---
+++ app/src/main/java/com/movies/tobi/popularmovies/popularstream/MoviePoster.java
@@ -0,0 +1,12 @@
+package com.movies.tobi.popularmovies.popularstream;
+
+public class MoviePoster {
+
+    private final long movieId;
+    private final String posterPath;
+
+    public MoviePoster(long movieId, String posterPath) {
+        this.movieId = movieId;
+        this.posterPath = posterPath;
+    }
+}
