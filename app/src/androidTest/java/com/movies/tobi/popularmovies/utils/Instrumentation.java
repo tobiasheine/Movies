387a30f27d384c9606bd19b085afd1e7e4986648
Tobias Heine <tobias@novoda.com>
2016-05-18 19:42:40 +0100

    switch instrumentation based on project properties

--- app/src/androidTest/java/com/movies/tobi/popularmovies/utils/Instrumentation.java
+++ app/src/androidTest/java/com/movies/tobi/popularmovies/utils/Instrumentation.java
@@ -3,14 +3,25 @@ package com.movies.tobi.popularmovies.utils;
 import android.os.Bundle;
 import android.support.test.runner.AndroidJUnitRunner;
 
+import com.movies.tobi.popularmovies.BuildConfig;
+
 import cucumber.api.android.CucumberInstrumentationCore;
 
 public class Instrumentation extends AndroidJUnitRunner {
+
+    private static final String CUCUMBER_TAGS_KEY = "tags";
 
     private final CucumberInstrumentationCore instrumentationCore = new CucumberInstrumentationCore(this);
 
     @Override
     public void onCreate(final Bundle bundle) {
+        final String tags = BuildConfig.TEST_TAGS;
+        if (!tags.isEmpty()) {
+            // Reformat tags list to separate items with '--' as expected by Cucumber library, see method
+            // cucumber-android-1.2.2.jar\cucumber\runtime\android\Arguments.class @ appendOption()
+            bundle.putString(CUCUMBER_TAGS_KEY, tags.replaceAll(",", "--").replaceAll("\\s", ""));
+        }
+
         instrumentationCore.create(bundle);
         super.onCreate(bundle);
     }
