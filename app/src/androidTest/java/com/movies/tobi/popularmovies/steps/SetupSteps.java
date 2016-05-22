e94b298952ce77c906f15f0035c7d686352e7bf7
Tobias Heine <tobias@novoda.com>
2016-05-22 17:33:19 +0200

    Add delay before app start

--- app/src/androidTest/java/com/movies/tobi/popularmovies/steps/SetupSteps.java
+++ app/src/androidTest/java/com/movies/tobi/popularmovies/steps/SetupSteps.java
@@ -38,6 +38,12 @@ public class SetupSteps {
 
     @Given("^I start the application$")
     public void I_start_app() {
+        //FIXME: otherwise when running multiple tests after each other sth. the instrumentation times out when starting the activity
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         mActivityRule.launchActivity(null);
     }
 
