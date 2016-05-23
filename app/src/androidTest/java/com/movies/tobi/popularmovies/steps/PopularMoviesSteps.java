cf02eac125047e1f973dec1cb2e2496b0ff86217
Tobias Heine <tobias@novoda.com>
2016-05-23 08:23:19 +0200

    Create test to assert which posters are displayed

--- app/src/androidTest/java/com/movies/tobi/popularmovies/steps/PopularMoviesSteps.java
+++ app/src/androidTest/java/com/movies/tobi/popularmovies/steps/PopularMoviesSteps.java
@@ -3,6 +3,7 @@ package com.movies.tobi.popularmovies.steps;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 
 import com.movies.tobi.popularmovies.R;
+import com.movies.tobi.popularmovies.matchers.PosterMatcher;
 
 import java.util.Map;
 
@@ -19,9 +20,20 @@ import static org.hamcrest.core.AllOf.allOf;
 public class PopularMoviesSteps {
 
     @When("^I select the poster at position (\\d+)$")
-    public void I_select_a_poster(final int position) {
+    public void I_select_a_poster_at(final int position) {
         onView(withId(R.id.popularMovies_recycler))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(position, click()));
+    }
+
+    @When("^I expect to see the following movie posters$")
+    public void I_expect_to_see_movie_posters(final DataTable dataTable) {
+        for (Map<String, String> row : dataTable.asMaps(String.class, String.class)) {
+            int position = Integer.valueOf(row.get("position"));
+            String posterPath = row.get("posterPath");
+
+            onView(withId(R.id.popularMovies_recycler))
+                    .check(matches(PosterMatcher.hasPosterAt(position, posterPath)));
+        }
 
     }
 
