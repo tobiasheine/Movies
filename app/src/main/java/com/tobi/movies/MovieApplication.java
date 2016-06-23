9c0b24e8f20573346367cfc739117a891deccc1a
Tobias Heine <tobias@novoda.com>
2016-06-23 09:14:03 +0200

    Remove no more needed getDependencies method

--- app/src/main/java/com/tobi/movies/MovieApplication.java
+++ app/src/main/java/com/tobi/movies/MovieApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.support.annotation.VisibleForTesting;
 
 import com.tobi.movies.popularstream.PopularStreamRepository;
-import com.tobi.movies.posterdetails.MovieDetailsPresenter;
 import com.tobi.movies.posterdetails.MovieDetailsRepository;
 
 public class MovieApplication extends Application implements Dependencies {
@@ -35,10 +34,5 @@ public class MovieApplication extends Application implements Dependencies {
     @VisibleForTesting
     public void setDependencies(Dependencies dependencies) {
         this.dependencies = dependencies;
-    }
-
-    @VisibleForTesting
-    public Dependencies getDependencies() {
-        return dependencies;
     }
 }
