f54ff2509ea3f98596a9b74711ffe33955bc4bbf
Tobias Heine <tobias@novoda.com>
2017-01-20 12:29:43 +0100

    Remove not needed feature and step implementation

--- app/src/androidTest/java/com/tobi/movies/steps/PopularMoviesSteps.java
+++ app/src/androidTest/java/com/tobi/movies/steps/PopularMoviesSteps.java
@@ -1,30 +1,22 @@
 package com.tobi.movies.steps;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
 
 import com.tobi.movies.Dependencies;
 import com.tobi.movies.EspressoDependencies;
 import com.tobi.movies.MovieApplication;
 import com.tobi.movies.backend.ConfigurableBackend;
 import com.tobi.movies.popularstream.ApiMoviePoster;
-import com.tobi.movies.popularstream.PopularMoviesActivity;
 import com.tobi.movies.popularstream.PopularMoviesRobot;
 
 import java.util.Map;
 
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
 
 public class PopularMoviesSteps {
 
     private final PopularMoviesRobot popularMoviesRobot = PopularMoviesRobot.create();
-
-    @Given("^I start the application$")
-    public void I_start_app() {
-        popularMoviesRobot.launchPopularMovies(new ActivityTestRule<>(PopularMoviesActivity.class));
-    }
 
     @Given("^the following remote movie posters exist$")
     public void the_following_remote_movie_poster_exist(final DataTable dataTable) {
@@ -47,21 +39,6 @@ public class PopularMoviesSteps {
         poster.movieId = movieId;
         poster.posterPath = posterPath;
         return poster;
-    }
-
-    @When("^I select the poster at position (\\d+)$")
-    public void I_select_a_poster_at(final int position) {
-        popularMoviesRobot.selectPosterAtPosition(position);
-    }
-
-    @When("^I expect to see the following movie posters$")
-    public void I_expect_to_see_movie_posters(final DataTable dataTable) {
-        for (Map<String, String> row : dataTable.asMaps(String.class, String.class)) {
-            int position = Integer.valueOf(row.get("position"));
-            String posterPath = row.get("posterPath");
-
-            popularMoviesRobot.checkPosterWithPathIsDisplayedAtPosition(position, posterPath);
-        }
     }
 
     private ConfigurableBackend getConfigurableBackend() {
