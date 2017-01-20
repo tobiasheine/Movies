45b1cd5c4f36f0e0343bd5f3e84f70a65946cec8
Tobias Heine <tobias@novoda.com>
2017-01-20 11:59:26 +0100

    Prepare MovieDetailsSteps for kata

--- app/src/androidTest/java/com/tobi/movies/steps/MovieDetailsSteps.java
+++ app/src/androidTest/java/com/tobi/movies/steps/MovieDetailsSteps.java
@@ -1,30 +1,19 @@
 package com.tobi.movies.steps;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
 
 import com.tobi.movies.Dependencies;
 import com.tobi.movies.EspressoDependencies;
 import com.tobi.movies.MovieApplication;
 import com.tobi.movies.backend.ConfigurableBackend;
 import com.tobi.movies.posterdetails.ApiMovieDetails;
-import com.tobi.movies.posterdetails.MovieDetailsActivity;
-import com.tobi.movies.posterdetails.PosterDetailsRobot;
 
 import java.util.Map;
 
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 
 public class MovieDetailsSteps {
-
-    private final PosterDetailsRobot movieDetailsRobot = PosterDetailsRobot.create();
-
-    @Given("^I start the details screen with movie id (\\d+)$")
-    public void I_start_the_details_screen_with_movie(int movieId) {
-        movieDetailsRobot.launchDetailsScreen(movieId, new ActivityTestRule<>(MovieDetailsActivity.class));
-    }
 
     @Given("^the following remote movie details exist$")
     public void the_following_remote_movie_details_exist(final DataTable dataTable) {
@@ -54,21 +43,6 @@ public class MovieDetailsSteps {
         apiMovieDetails.posterPath = posterPath;
         apiMovieDetails.releaseDate = releaseDate;
         return apiMovieDetails;
-    }
-
-    @Then("^I expect to see the following movie details$")
-    public void I_expect_to_see_the_movie_details(final DataTable dataTable) {
-        if (dataTable.asMaps(String.class, String.class).size() > 1) {
-            throw new IllegalArgumentException("We can just display one movie per time");
-        }
-
-        final Map<String, String> row = dataTable.asMaps(String.class, String.class).get(0);
-
-        String movieTitle = row.get("movieTitle");
-        String movieDetails = row.get("movieDetails");
-
-        movieDetailsRobot.checkMovieTitleIsDisplayed(movieTitle);
-        movieDetailsRobot.checkMovieDescriptionIsDisplayed(movieDetails);
     }
 
     private ConfigurableBackend getConfigurableBackend() {
